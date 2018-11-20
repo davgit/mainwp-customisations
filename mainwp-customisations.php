b416da9ba066bca2508b48d8049c344c47f613a6
Thang Hoang Van <thang@mainwp.com>
2018-11-20 23:23:42 +0700

    Tweak a bit

--- mainwp-customisations.php
+++ mainwp-customisations.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 3.6
  * Tested up to:      5.0
- *  
+ *
  ************************************************************
  * This plugin has been inpired and built on:				*
  * Theme Customisations by WooThemes						*
@@ -32,54 +32,42 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0
  * @package	MainWP_Customisations
  */
-final class MainWP_Customisations {
 
-	/**
-	 * Set up the plugin
-	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'mainwp_customisations_setup' ), -1 );
-		require_once( 'custom/functions.php' );
-	}
+if (!class_exists('MainWP_Customisations')) {
+    final class MainWP_Customisations {
 
-	/**
-	 * Setup everything
-	 */
-	public function mainwp_customisations_setup() {
-		add_action( 'admin_enqueue_scripts', array( $this, 'mainwp_customisations_css' ), 999 );
-		add_action( 'admin_enqueue_scripts', array( $this, 'mainwp_customisations_js' ) );
-	}
+        /**
+         * Set up the plugin
+         */
+        public function __construct() {
+            require_once( 'custom/functions.php' );
+            add_action( 'admin_enqueue_scripts', array( $this, 'mainwp_customisations_css' ), 999 );
+            add_action( 'admin_enqueue_scripts', array( $this, 'mainwp_customisations_js' ) );
+        }
 
-	/**
-	 * Enqueue the Custom CSS
-	 *
-	 * @return void
-	 */
-	public function mainwp_customisations_css() {
-		wp_enqueue_style( 'mainwp-custom-style', plugins_url( '/custom/mainwp-custom-style.css', __FILE__ ) );
-	}
+        /**
+         * Enqueue the Custom CSS
+         *
+         * @return void
+         */
+        public function mainwp_customisations_css() {
+            wp_enqueue_style( 'mainwp-custom-style', plugins_url( '/custom/mainwp-custom-style.css', __FILE__ ) );
+        }
 
-	/**
-	 * Enqueue the Custom Javascript
-	 *
-	 * @return void
-	 */
-	public function mainwp_customisations_js() {
-		wp_enqueue_script( 'custom-mainwp-js', plugins_url( '/custom/mainwp-custom-js.js', __FILE__ ), array( 'jquery' ) );
-	}
+        /**
+         * Enqueue the Custom Javascript
+         *
+         * @return void
+         */
+        public function mainwp_customisations_js() {
+            wp_enqueue_script( 'custom-mainwp-js', plugins_url( '/custom/mainwp-custom-js.js', __FILE__ ), array( 'jquery' ) );
+        }
 
-} // End Class
+    } // End Class
 
-/**
- * Main function
- *
- * @return void
- */
-function start_mainwp_customisations() {
-	new MainWP_Customisations();
 }
 
 /**
  * Initialise the plugin
  */
-add_action( 'plugins_loaded', 'start_mainwp_customisations' );
+new MainWP_Customisations();
